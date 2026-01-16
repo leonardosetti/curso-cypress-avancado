a79fb7ee1bd45a962dba029f903841c7bc89921a
Leonardo Setti <leonardosetti@protonmail.com>
2026-01-16 18:15:26 -0300

    bak in the roots

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: '6stktc',
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
+    setupNodeEvents (on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'https://wlsf82-hacker-stories.web.app',
-  },
+    baseUrl: 'https://wlsf82-hacker-stories.web.app'
+  }
 })
